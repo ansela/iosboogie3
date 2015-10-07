67c5cc2da68d62250ded36fe14db4fd9ba6cba01
ansela <ashley.ansell@gmail.com>
2015-10-07 19:03:49 -0400

    Initial Commit

---
+++ iosboogie5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iosboogie5
+//
+//  Created by Ashley Ansell on 10/7/15.
+//  Copyright © 2015 Ashley Ansell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
